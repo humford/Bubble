10450b2cdce38342c0eba8f93821178994a4e6aa
LieutenantHawkeye <hwill12345@gmail.com>
2015-08-25 15:30:05 -0400

    cloned

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source "https://rubygems.org"
+
+gem "sinatra"
+
+group :development do
+  gem "shotgun"
+  gem "pry"
+  gem "tux"
+end
---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+require "./config/environment"
+
+class ApplicationController < Sinatra::Base
+  configure do
+    set :public_folder, "public"
+    set :views, "app/views"
+  end
+
+  get "/" do
+    "Hello World!"
+  end
+end
+
