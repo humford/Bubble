b132d42d1c55b89943407adb62e7cca19a75f7c3
LieutenantHawkeye <hwill12345@gmail.com>
2015-08-26 11:29:37 -0400

    required modes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 require "./config/environment"
+require "./app/models/bubble_post"
+require "./app/models/bubble"
+require "./app/models/comment"
+require "./app/models/membership"
+require "./app/models/post"
+require "./app/models/user"
 
 class ApplicationController < Sinatra::Base
   configure do
