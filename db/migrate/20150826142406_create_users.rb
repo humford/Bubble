09145926d187a77c4704a2c0f6dfd42e89d674e3
LieutenantHawkeye <hwill12345@gmail.com>
2015-08-26 10:40:43 -0400

    made users and posts migrations

--- db/migrate/20150826142406_create_users.rb
+++ db/migrate/20150826142406_create_users.rb
@@ -1,4 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
+	  create_table :users do |t|
+		  t.string :username
+		  t.string :password
+		  t.string :realname
+		  t.string :email
+		  t.datetime :account_created
+		  t.string :phone
+		  t.integer :score
   end
 end
